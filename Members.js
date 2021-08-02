76a499963b921df92dc4eb7f2fd18df76e0229c5
Chathura Prabasha <chathuraprabasha@gmail.com>
2021-08-03 00:07:41 +0530

    custom error message added

---
+++ Members.js
@@ -0,0 +1,21 @@
+const members = [
+  {
+    id: 1,
+    name: 'Chathura',
+    email: 'chathura@mail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Lakshan',
+    email: 'Lakshan@mail.com',
+    status: 'active',
+  },
+  {
+    id: 3,
+    name: 'Tharindu',
+    email: 'Tharindu@mail.com',
+    status: 'active',
+  },
+];
+module.exports = members;
